0a93319548954293fda4dd10eb630546b70cdd2d
moontai0724 <moontai0724@gmail.com>
2022-01-13 06:51:21 +0800

    feat: update whole file when change content

--- pages/edit/_id.vue
+++ pages/edit/_id.vue
@@ -2,12 +2,10 @@
   <div class="code">
     <div class="codemirror">
       <codemirror
+        ref="cmEditor"
         v-model="code"
         :options="cmOptions"
-        @cursorActivity="onCmCursorActivity"
-        @ready="onCmReady"
-        @focus="onCmFocus"
-        @blur="onCmBlur"
+        @input="onCmCodeChange"
       />
     </div>
     <pre class="pre">{{ code }}</pre>
@@ -17,52 +15,56 @@
 <script lang="ts">
 import Vue from "vue";
 import { Manager, Socket } from "socket.io-client";
-// import { codemirror } from "vue-codemirror";
+import { codemirror } from "vue-codemirror";
 
-// // base style
-// import "codemirror/lib/codemirror.css";
+// base style
+import "codemirror/lib/codemirror.css";
 
-// // theme css
-// import "codemirror/theme/base16-dark.css";
+// theme css
+import "codemirror/theme/base16-dark.css";
 
-// // language
-// import "codemirror/mode/vue/vue.js";
+// language
+import "codemirror/mode/vue/vue.js";
 
-// // active-line.js
-// import "codemirror/addon/selection/active-line.js";
+// active-line.js
+import "codemirror/addon/selection/active-line.js";
 
-// // styleSelectedText
-// import "codemirror/addon/selection/mark-selection.js";
-// import "codemirror/addon/search/searchcursor.js";
+// styleSelectedText
+import "codemirror/addon/selection/mark-selection.js";
+import "codemirror/addon/search/searchcursor.js";
 
-// // highlightSelectionMatches
-// import "codemirror/addon/scroll/annotatescrollbar.js";
-// import "codemirror/addon/search/matchesonscrollbar.js";
-// import "codemirror/addon/search/match-highlighter.js";
+// highlightSelectionMatches
+import "codemirror/addon/scroll/annotatescrollbar.js";
+import "codemirror/addon/search/matchesonscrollbar.js";
+import "codemirror/addon/search/match-highlighter.js";
 
-// // keyMap
-// import "codemirror/mode/clike/clike.js";
-// import "codemirror/addon/edit/matchbrackets.js";
-// import "codemirror/addon/comment/comment.js";
-// import "codemirror/addon/dialog/dialog.js";
-// import "codemirror/addon/dialog/dialog.css";
-// import "codemirror/addon/search/search.js";
-// import "codemirror/keymap/sublime.js";
+// keyMap
+import "codemirror/mode/clike/clike.js";
+import "codemirror/addon/edit/matchbrackets.js";
+import "codemirror/addon/comment/comment.js";
+import "codemirror/addon/dialog/dialog.js";
+import "codemirror/addon/dialog/dialog.css";
+import "codemirror/addon/search/search.js";
+import "codemirror/keymap/sublime.js";
 
-// // foldGutter
-// import "codemirror/addon/fold/foldgutter.css";
-// import "codemirror/addon/fold/brace-fold.js";
-// import "codemirror/addon/fold/comment-fold.js";
-// import "codemirror/addon/fold/foldcode.js";
-// import "codemirror/addon/fold/foldgutter.js";
-// import "codemirror/addon/fold/indent-fold.js";
-// import "codemirror/addon/fold/markdown-fold.js";
-// import "codemirror/addon/fold/xml-fold.js";
+// foldGutter
+import "codemirror/addon/fold/foldgutter.css";
+import "codemirror/addon/fold/brace-fold.js";
+import "codemirror/addon/fold/comment-fold.js";
+import "codemirror/addon/fold/foldcode.js";
+import "codemirror/addon/fold/foldgutter.js";
+import "codemirror/addon/fold/indent-fold.js";
+import "codemirror/addon/fold/markdown-fold.js";
+import "codemirror/addon/fold/xml-fold.js";
 
 export default Vue.extend({
   name: "EditPage",
+  components: {
+    codemirror,
+  },
   data() {
     return {
+      id: this.$route.params.id,
       title: "",
       code: "",
       manager: {} as Manager,
@@ -78,18 +80,17 @@ export default Vue.extend({
             if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
           },
         },
+        line: true,
         lineWrapping: true,
         lineNumbers: true,
         readOnly: false,
         showCursorWhenSelecting: true,
         lineWiseCopyCut: true,
         autofocus: true,
+        viewportMargin: 20,
       },
     };
   },
-  // components: {
-  //   codemirror,
-  // },
   mounted() {
     this.manager = new Manager("ws://localhost", {
       reconnectionDelayMax: 10000,
@@ -99,9 +100,13 @@ export default Vue.extend({
       },
     });
     this.currentSocket = this.manager.socket("/");
-    this.currentSocket.emit("OpenFile", { id: this.$route.params.id });
+    this.currentSocket.emit("OpenFile", { id: this.id });
     this.currentSocket.on("RequestedFile", (file) => {
       if (!file) this.$router.push("/");
+      this.code = file.content;
+      this.title = file.title;
+    });
+    this.currentSocket.on("FileUpdated", (file) => {
       this.code = file.content;
       this.title = file.title;
     });
@@ -110,17 +115,13 @@ export default Vue.extend({
     this.currentSocket.disconnect();
   },
   methods: {
-    onCmCursorActivity(codemirror) {
-      console.debug("onCmCursorActivity", codemirror);
-    },
-    onCmReady(codemirror) {
-      console.debug("onCmReady", codemirror);
-    },
-    onCmFocus(codemirror) {
-      console.debug("onCmFocus", codemirror);
-    },
-    onCmBlur(codemirror) {
-      console.debug("onCmBlur", codemirror);
+    onCmCodeChange(newCode) {
+      console.log("this is new code", newCode);
+      this.currentSocket.emit("UpdateFile", {
+        id: this.id,
+        title: this.title,
+        content: newCode,
+      });
     },
   },
 });
@@ -141,6 +142,8 @@ export default Vue.extend({
 
   .pre {
     display: block;
+    height: 100%;
+    overflow: auto;
     padding: 1rem;
     font-size: 12px;
     line-height: 1.6;
