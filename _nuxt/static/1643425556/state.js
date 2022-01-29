fb1350aab06d927a994b22cdbd1d51345520c1c4
moontai0724 <moontai0724@gmail.com>
2022-01-29 03:05:57 +0000

    Deploying to gh-pages from @ moontai0724/online-editor-frontend@1084d8c7f27079b6212b679f442bc46b4d286d00 🚀

---
+++ _nuxt/static/1643425556/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fonline-editor-frontend\u002F_nuxt\u002Fstatic\u002F1643425556",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{WS_HOST:"moontai0724-online-editor.herokuapp.com",_app:{basePath:"\u002Fonline-editor-frontend\u002F",assetsPath:"\u002Fonline-editor-frontend\u002F_nuxt\u002F",cdnURL:a}}}}(null));
